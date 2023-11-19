ebac3fb557f025e52243f68173470a1938922a6f
Fernando Soto <fernando.soto@epn.edu.ec>
2023-11-19 13:42:44 -0500

    feat: adding logic for get user data

--- src/main/java/org/example/CleanedData.java
+++ src/main/java/org/example/CleanedData.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class CleanedData {
+    public boolean clean(String userPlate, String userDate, String userHour) {
+        return false;
+    }
     // function data cleaning
     //--> Validate
     //--> Format
--- src/main/java/org/example/View.java
+++ src/main/java/org/example/View.java
@@ -1,13 +1,37 @@
 package org.example;
 
 
+import java.util.Scanner;
+
 public class View {
+    Scanner scanner = new Scanner(System.in);
     CleanedData cleaner = new CleanedData();
     Predictor predictor = new Predictor(this);
     public void getUserData() {
-        // Recieve user data
-        // Invoque cleaning data
-        // Send cleanded data to Predictor class
+        // Receive user data until the data is cleaned
+        String userPlate;
+        String userDate;
+        String userHour;
+        do {
+            userPlate = readString("Enter the licence plate (ex: PBU-1234): ");
+            userDate = readString("Enter the date (ex: 11-18-2023): ");
+            userHour = readString("Enter the time: (19:00): ");
+
+        } while (!cleaner.clean(userPlate, userDate, userHour));
+        
+        // Send cleaned data to Predictor class
+        sendCleanedData(cleaner);
     }
-    // printResutl()
+
+    private void sendCleanedData(CleanedData cleaner) {
+    }
+
+
+    // printResult()
+    
+    // Function for read user input
+    private String readString(String message){
+        System.out.print(message);
+        return scanner.next();
+    }
 }
