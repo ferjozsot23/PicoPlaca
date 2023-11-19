c192b8ce374f19d5355bada70dec48f454728352
Fernando Soto <fernando.soto@epn.edu.ec>
2023-11-19 13:54:58 -0500

    feat: create logic for verify a valid license plate

--- src/main/java/org/example/CleanedData.java
+++ src/main/java/org/example/CleanedData.java
@@ -1,10 +1,33 @@
 package org.example;
 
+import java.text.ParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CleanedData {
+
+    // Cleaning involves validate and format the data
     public boolean clean(String userPlate, String userDate, String userHour) {
-        return false;
+        if (!isValidData(userPlate, userDate, userHour)) {
+            return false;
+        }
+        return true;
     }
-    // function data cleaning
-    //--> Validate
-    //--> Format
+
+    private boolean isValidData(String userPlate, String userDate, String userHour) {
+        return isValidPlate(userPlate);
+    }
+
+    public boolean isValidPlate(String userPlate) {
+        if (userPlate == null) return false;
+
+        String pattern = "^[A-Z]{3}-[0-9]{3,4}$";
+
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(userPlate);
+
+        return matcher.matches();
+    }
+
+
 }
--- src/test/java/org/example/CleanedDataTest.java
+++ src/test/java/org/example/CleanedDataTest.java
@@ -1,4 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CleanedDataTest {
-  
+    private CleanedData cleanedData;
+    @BeforeEach
+    public void setUp() {
+        cleanedData = new CleanedData();
+    }
+    @org.junit.jupiter.api.Test
+    void isValidPlate() {
+        assertTrue(cleanedData.isValidPlate("PBU-1234"));
+        assertTrue(cleanedData.isValidPlate("PBU-123"));
+    }
 }
